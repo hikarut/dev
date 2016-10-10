8f3a8426015b8354cefc2acc6724f225c362d8ed
HikaruTakahashi <seven_star_dust@yahoo.co.jp>
2016-10-10 11:20:31 +0900

    mod design

--- timer/js/custom.js
+++ timer/js/custom.js
@@ -1,6 +1,8 @@
 $(function(){
     $('.start').click(function() {
         $('.start').html('');
+        $('.timer').html('');
+
         $('.timer').startTimer({
             onComplete: function() {
                 $('.timer').html('終了です');
